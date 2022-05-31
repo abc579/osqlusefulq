147e2dd58ca6cb2c79a809b37a483dc7e1b503fe
abc579 <73013256+abc579@users.noreply.github.com>
2022-05-31 08:12:53 +0200

    Create ublocks.sql
    Blocks between users.

---
+++ ublocks.sql
@@ -0,0 +1,15 @@
+/* Check blocks between users. */
+SELECT s1.username || '@' || s1.machine || ' ( SID=' || s1.sid || ' SERIAL=' ||
+       s1.serial# || ' ) esta bloqueando ' || s2.username || '@' || s2.machine ||
+       ' ( SID=' || s2.sid || ' SERIAL=' || s2.serial# || ' ) ' AS bloqueos
+  FROM v$lock    l1
+      ,v$session s1
+      ,v$lock    l2
+      ,v$session s2e
+
+ WHERE s1.sid = l1.sid
+   AND s2.sid = l2.sid
+   AND l1.block = 1
+   AND l2.request > 0
+   AND l1.id1 = l2.id1
+   AND l2.id2 = l2.id2;
